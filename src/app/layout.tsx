142c25bc19a7e06644bebc8b8c988100523504c1
kanglabib <labibmoe13@gmail.com>
2026-08-27 11:46:38 +0700

    Resolve Program

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import "./globals.css";
 
 const geistSans = localFont({
